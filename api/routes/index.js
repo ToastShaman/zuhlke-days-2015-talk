1b547529e637fd866145fe8ff3fb694c46ec5407
Kevin <kevin.denver@gmail.com>
2015-06-03 18:07:13 +0100

    fix(api): headers can not be set after they have been sent error

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,38 +2,45 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 
+function invalidUsernameOrPassword() {
+  var err = new Error('Invalid username/password');
+  err.status = 401;
+  return err;
+}
+
+function user(existingUser) {
+  return {
+    user: existingUser,
+    accessToken: jwt.sign({username: existingUser.username}, 'secretKey')
+  };
+}
+
+function findOne(username, db) {
+  return db.findOne({'username': username});
+}
+
 router.post('/login', function(req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
   var accessToken = req.get('X-Auth-Token');
+  var db = req.app.locals.users;
 
   if (accessToken) {
     jwt.verify(accessToken, 'secretKey', function(err, decoded) {
-      if (!err) {
-        var existingUser = req.app.locals.users.findOne({'username': decoded.username});
-        if (existingUser) {
-          return res.json({
-            user: existingUser,
-            accessToken: jwt.sign({username: existingUser.username}, 'secretKey')
-          });
-        }
-      }
+      if (err) return next(invalidUsernameOrPassword());
+      var existingUser = findOne(decoded.username, db);
+      if (existingUser) return res.json(user(existingUser));
+      return next(invalidUsernameOrPassword());
     });
   }
-
-  if (username && password) {
-    var existingUser = req.app.locals.users.findOne({'username': username});
-    if (existingUser && existingUser.password === password) {
-      return res.json({
-        user: existingUser,
-        accessToken: jwt.sign({username: existingUser.username}, 'secretKey')
-      });
-    }
+  else if (username && password) {
+    var existingUser = findOne(username, db);
+    if (existingUser && existingUser.password === password) return res.json(user(existingUser));
+    return next(invalidUsernameOrPassword());
   }
-
-  var err = new Error('Invalid username/password');
-  err.status = 401;
-  next(err);
+  else {
+    return next(invalidUsernameOrPassword());
+  }
 });
 
 module.exports = router;
