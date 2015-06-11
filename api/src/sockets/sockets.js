0dfd765bbdbbf0fae8c0c44b337162bce6ed9466
Kevin <kevin.denver@gmail.com>
2015-06-11 14:56:25 +0200

    fix(api memory leak): do not create new listeners for all io connections

--- api/src/sockets/sockets.js
+++ api/src/sockets/sockets.js
@@ -1,10 +1,8 @@
 var sms = require('./../events/sms');
 
 var sockets = function(io) {
-  io.on('connection', function(socket) {
-    sms.on('received', function(message) {
-      socket.emit('receivedSms', message);
-    })
+  sms.on('received', function(message) {
+    io.emit('receivedSms', message);
   });
 };
 
